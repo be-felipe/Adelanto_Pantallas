0a3086a3b763b461da96cd1afd8716aa184d5994
Andres Felipe Becerra Barrera <an-becerra@javeriana.edu.co>
2021-08-23 21:33:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/adelanto_pantallas/Cofig.java
@@ -0,0 +1,14 @@
+package com.example.adelanto_pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cofig extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cofig);
+    }
+}
---
+++ app/src/main/java/com/example/adelanto_pantallas/PantallaConfirmacion.java
@@ -0,0 +1,14 @@
+package com.example.adelanto_pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaConfirmacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_confirmacion);
+    }
+}
